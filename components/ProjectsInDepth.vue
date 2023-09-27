0b3ddec437c9a13136c7261a6bd0a7a88f4e6110
Kakackle <markowskimaciej98@gmail.com>
2023-09-27 19:45:31 +0200

    projects home json data and display

--- components/ProjectsInDepth.vue
+++ components/ProjectsInDepth.vue
@@ -1,37 +1,5 @@
 <script setup>
-const projects=[
-    {
-        title: 'Django Instagram clone (with HTMX)',
-        main_features: [
-            {
-                img: "projects/Insta/Home.PNG",
-                desc: "Infinite scroll post page with comments section previews, post liking, filtering by followed users etc"
-            },
-            {
-                img: "projects/Insta/User_view.PNG",
-                desc: "User profiles with followers, created posts, editing, authorization"
-            },
-            {
-                img: "projects/Insta/post_view.PNG",
-                desc: "..."
-            }
-        ],
-        steps: [
-            {
-                img: "projects/Insta/Grid_liked.PNG",
-                desc: "Filtering posts by tags, description, following or being liked"
-            },
-            {
-                img: "projects/Insta/New_post_form.PNG",
-                desc: "Django forms with bootstrap styling"
-            },
-            {
-                img: "projects/Insta/Edit_profile.PNG",
-                desc: "Editing...."
-            }
-        ]
-    },
-]
+import projects from "../projects.json"
 </script>
 
 <template>
